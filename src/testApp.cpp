c7f293463bf2d8bd7d9926ed4dedac65aaa968f2
natxopedreira <natxopedreira@gmail.com>
2012-09-27 12:01:52 +0200

    retoques

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -53,26 +53,6 @@ void testApp::creaGui(){
     gui->addToggle("useSmoothing", false, 40, 40, OFX_UI_FONT_SMALL);
     gui->addButton("saveSettings", false, 40, 40, OFX_UI_FONT_SMALL);
     gui->addButton("saveCalibration", false, 40, 40, OFX_UI_FONT_SMALL);
-    /*
-	shadingModes.push_back("none");
-	shadingModes.push_back("lights");
-	shadingModes.push_back("shader");
-    gui->addSpacer(length-xInit, 1);
-    gui->addRadio("SHADING", shadingModes, OFX_UI_ORIENTATION_VERTICAL, dim, dim);
-    */
-    
-    /*
-     NO SE PARA QUE VALE ESO DE HIGHLIGHT
-    gui->addSpacer(length-xInit, 1);
-    gui->addWidgetDown(new ofxUILabel("HIGHLIGHT", OFX_UI_FONT_MEDIUM));
-    
-    gui->addToggle("highlight", false, dim, dim, OFX_UI_FONT_SMALL);
-    gui->addSlider("highlightPosition", 0.0, 1.0, 0.0, length-xInit, dim, OFX_UI_FONT_SMALL);
-    gui->addSlider("highlightOffset", 0.0, 1.0, 0.1, length-xInit, dim, OFX_UI_FONT_SMALL);
-    */
-    //gui->addSpacer(length-xInit, 1);
-    
-
     parsearInt.push_back("lineWidth");
     
     
@@ -80,7 +60,7 @@ void testApp::creaGui(){
     gui2->setDrawBack(false);
     gui2->addWidgetDown(new ofxUILabel("CALIBRATION", OFX_UI_FONT_MEDIUM));
     gui2->addSlider("aov", 50, 100, 80, length-xInit, dim, OFX_UI_FONT_SMALL);
-    parsearInt.push_back("aov");
+    //parsearInt.push_back("aov");
     gui2->addToggle("cv_calib_fix_aspect_ratio", true, dim, dim, OFX_UI_FONT_SMALL);
     gui2->addToggle("cv_calib_fix_k1", true, dim, dim, OFX_UI_FONT_SMALL);
     gui2->addToggle("cv_calib_fix_K2", true, dim, dim, OFX_UI_FONT_SMALL);
@@ -88,25 +68,14 @@ void testApp::creaGui(){
     gui2->addToggle("cv_calib_zero_tangent_dist", true, dim, dim, OFX_UI_FONT_SMALL);
     gui2->addToggle("cv_calib_fix_principal_point", false, dim, dim, OFX_UI_FONT_SMALL);
     
-    /*
-    gui2->addSpacer(length-xInit, 1);
-    gui2->addWidgetDown(new ofxUILabel("RENDER", OFX_UI_FONT_MEDIUM));
-    */
-    
-    
     gui2->addSlider("screenPointSize", 1.0, 16.0, 2.0, length-xInit, dim, OFX_UI_FONT_SMALL);
     gui2->addSlider("selectedPointSize", 1.0, 16.0, 8.0, length-xInit, dim, OFX_UI_FONT_SMALL);
     gui2->addSlider("selectionRadius", 1.0, 32.0, 12.0, length-xInit, dim, OFX_UI_FONT_SMALL);
     parsearInt.push_back("screenPointSize");
     parsearInt.push_back("selectedPointSize");
     parsearInt.push_back("selectionRadius");
-    
     gui2->addSpacer(length-xInit, 1);
-    /*gui2->addToggle("useFog", false, dim, dim, OFX_UI_FONT_SMALL);
-    gui2->addSlider("fogNear", 0.0, 1000.0, 200.0, length-xInit, dim, OFX_UI_FONT_SMALL);
-    gui2->addSlider("fogFar", 0.0, 2500.0, 1850, length-xInit, dim, OFX_UI_FONT_SMALL);
-    gui2->addSpacer(length-xInit, 1);*/
-    
+
     gui2->addSlider("lightX", -1000, 1000, 200, length-xInit, dim, OFX_UI_FONT_SMALL);
     gui2->addSlider("lightY", -1000, 1000, 400, length-xInit, dim, OFX_UI_FONT_SMALL);
     gui2->addSlider("lightZ", -1000, 1000, 800, length-xInit, dim, OFX_UI_FONT_SMALL);
@@ -150,7 +119,6 @@ void testApp::guiEvent(ofxUIEventArgs &e)
                 int valor = (int)floatValor->getScaledValue();
                 m.setAddress("/mapamoko/"+name+"/");
                 m.addIntArg(valor);
-                cout << valor << endl;
             }else{
                 float valor = floatValor->getScaledValue();
                 m.setAddress("/mapamoko/"+name+"/");
@@ -180,44 +148,4 @@ void testApp::keyPressed(int key){
         gui->saveSettings("gui.xml");
         gui2->saveSettings("gui2.xml");
     }
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
--- src/testApp.h
+++ src/testApp.h
@@ -13,16 +13,7 @@ class testApp : public ofBaseApp{
 		void draw();
         void creaGui();
         void guiEvent(ofxUIEventArgs &e);
-
 		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
 		
     
     ofxUICanvas *gui, *gui2, *gui3;
